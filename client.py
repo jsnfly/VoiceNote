d99feef01443db64a77fb229e74106fb0b44f17e
jsnfly <jsnfly@gmx.de>
2022-10-05 21:17:36 +0200

    restart project

--- client.py
+++ client.py
@@ -1,56 +1,58 @@
-import argparse
-import pyaudio
 import socket
 import time
+import pyaudio
 
-parser = argparse.ArgumentParser(description='Speech to text client.')
-parser.add_argument('--host')
-parser.add_argument('--port', type=int)
-parser.add_argument('--input_device_index', type=int, default=None)
-args = parser.parse_args()
+FORMAT = pyaudio.paInt16  # https://en.wikipedia.org/wiki/Audio_bit_depth
 
-CHUNK = 4096
-FORMAT = pyaudio.paInt16
+# TODO: get from device_info?
 CHANNELS = 1
 RATE = 44_100
 
-audio = pyaudio.PyAudio()
+HOST = socket.gethostname()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    def callback(in_data, frame_count, time_info, status):
-        message = pyaudio.paContinue
-        try:
-            sock.sendall(in_data)
-            # prediction = sock.recv(4096)
-            # if prediction:
-            #     print("Prediction:", prediction)
-            #     message = pyaudio.paComplete
-        except BrokenPipeError:
-            message = pyaudio.paComplete
-        return (in_data, message)
+
+def connect(sock):
     while True:
         try:
-            sock.connect((args.host, args.port))
-            print('Connected')
+            sock.connect((HOST, 12345))
+            print('Connected.')
             break
         except ConnectionRefusedError:
-            print('Trying to connect.')
+            print('Trying to connect...')
             time.sleep(1.0)
-    stream = audio.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK,
-        input_device_index=args.input_device_index,
-        stream_callback=callback
-    )
-    print("* recording")
 
-    while stream.is_active():
-        time.sleep(0.01)
 
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-    
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        connect(sock)
+
+        def _callback(in_data, frame_count, time_info, status):
+            message = pyaudio.paContinue
+            try:
+                sock.sendall(in_data)
+            except BrokenPipeError:
+                message = pyaudio.paComplete
+            return (in_data, message)
+
+        stream = audio.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=0,
+            input_device_index=None,
+            stream_callback=_callback
+        )
+
+        print("* recording")
+        while stream.is_active():
+            time.sleep(0.1)
+
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+
+if __name__ == '__main__':
+    audio = pyaudio.PyAudio()
+    main()
--- server.py
+++ server.py
@@ -1,117 +1,62 @@
 import socket
-import time
 import wave
 import pyaudio
-import torch
-import torchaudio
 import numpy as np
-from pathlib import Path
-from functools import cached_property
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
-
-
-HOST = '0.0.0.0'
-PORT = 65432
-
-WAVE_OUTPUT_FILENAME = "output.wav"
-MODEL = 'jonatasgrosman/wav2vec2-large-xlsr-53-german'
-RATE = 44_100
-CHUNK = 4096
-CHANNELS = 1
-FORMAT = pyaudio.paInt16
+import time
 
 audio = pyaudio.PyAudio()
 
-class Frame:
-
-    def __init__(self, binary_data):
-        self._data = binary_data
-
-    @property
-    def data(self):
-        return self._data
-
-    @cached_property
-    def numpy(self):
-        return np.frombuffer(self._data, dtype=np.int16)
-
-    @cached_property
-    def std(self):
-        return self.numpy.std()
+# TODO: get from client?
+PORT = 12345
+CHANNELS = 1
+RATE = 44100
+FORMAT = pyaudio.paInt16
 
 class Sample:
 
-    def __init__(self, frames):
-        self.frames = frames
-        self.frames_per_second = RATE / CHUNK
-        self.num_seconds_to_stop = 2
+    def __init__(self, fragments=[]):
+        self.fragments = fragments
+
+    def append(self, fragments):
+        self.fragments.append(fragments)
+
+    def numpy(self):
+        return np.frombuffer(b''.join(self.fragments), dtype=np.int16)
+
+    def is_finished(self):
+        pass
+
+    def is_empty(self):
+        pass
 
     def to_wav_file(self, file_path):
         wf = wave.open(file_path, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(audio.get_sample_size(FORMAT))
         wf.setframerate(RATE)
-        wf.writeframes(b''.join([f.data for f in self.frames]))
+        wf.writeframes(b''.join(self.fragments))
         wf.close()
 
-    def to_numpy(self):
-        return np.concatenate([f.numpy for f in self.frames])
 
-    def __add__(self, other):
-        return self.__class__(self.frames + other.frames)
-
-    def append(self, frame):
-        self.frames.append(frame)
-
-    def is_finished(self):
-        num_empty_frames_to_stop = int(self.frames_per_second * self.num_seconds_to_stop)
-        if len(self.frames) < num_empty_frames_to_stop:
-            return False
-        return self.frames_are_empty(self.frames[-num_empty_frames_to_stop:])
-
-    def is_empty(self):
-        return self.frames_are_empty(self.frames)
-
-    @staticmethod
-    def frames_are_empty(frames):
-        std_dev = np.array([f.std for f in frames]).std()
-
-        # TODO: Parameter
-        return std_dev < 15
+def predict():
+    time.sleep(0.1)
 
 
-def save_audio_and_prediction(save_path, sample, prediction):
-    save_path = Path(save_path) / time.strftime("%Y%m%d-%H%M%S")
-    save_path.mkdir(parents=True)
-    with open(save_path / 'prediction.txt', 'w') as f:
-        f.write(prediction)
-    sample.to_wav_file(str(save_path / 'sample.wav'))
+def main():
+    sample = Sample()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((socket.gethostname(), PORT))
+        sock.listen()
+        conn_sock, conn_addr = sock.accept()
+        with conn_sock:
+            print('Connected by', conn_addr)
+            time.sleep(0.1)
+            for i in range(20):
+                sample.append(conn_sock.recv(2**17, socket.MSG_DONTWAIT))
+                predict()
+            sample.to_wav_file('test.wav')
 
-processor = Wav2Vec2Processor.from_pretrained(MODEL)
-model = Wav2Vec2ForCTC.from_pretrained(MODEL)
-resambler = torchaudio.transforms.Resample(RATE, 16_000)
 
-current_sample = Sample([])
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen()
-    conn, addr = sock.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            num_bytes_per_chunk = CHUNK * 2  # Since each datapoint has 16 bits.
-            current_sample.append(Frame(conn.recv(CHUNK * 2, socket.MSG_WAITALL)))
-            if current_sample.is_finished():
-                if not current_sample.is_empty():
-                    data = current_sample.to_numpy()
-                    if RATE != 16_000:
-                        data = resambler(torch.Tensor(data)).numpy().squeeze()
-                    inputs = processor(data, sampling_rate=16_000, return_tensors="pt", padding=True)
-                    with torch.no_grad():
-                        logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
-                    predicted_ids = torch.argmax(logits, dim=-1)
-                    prediction = processor.batch_decode(predicted_ids)[0]
-                    print("Prediction:", prediction)
-                    save_audio_and_prediction('outputs', current_sample, prediction)
-                    conn.sendall(bytes(prediction, 'utf-8'))
-                current_sample = Sample([])
+
+if __name__ == '__main__':
+    main()
