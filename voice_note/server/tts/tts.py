b15ef932b3e849c7007adc0b8bf3a7835d0acea1
jsnfly <jsnfly@gmx.de>
2025-03-08 15:35:03 +0100

    use xtts-runner; update requirements

--- voice_note/server/tts/tts.py
+++ voice_note/server/tts/tts.py
@@ -1,43 +1,92 @@
 import asyncio
 import re
-import numpy as np
-from TTS.tts.configs.xtts_config import XttsConfig
-from TTS.tts.models.xtts import Xtts
-from typing import List
+import sys
+import torch
+from pathlib import Path
+from transformers.generation.streamers import BaseStreamer
+from typing import Dict, List
 
 from server.base_server import BaseServer, ThreadExecutor
 from server.utils.message import Message
-from server.utils.streaming_connection import POLL_INTERVAL
+from server.utils.streaming_connection import StreamingConnection
+
+sys.path.append('server/tts/xtts-runner/src')
+from gpt import XTTSGPT
+from tokenizer import TextTokenizer
 
 TTS_MODEL = './models/XTTS-v2'
 LANG = 'de'
-SAMPLE = f"{TTS_MODEL}/samples/{LANG}_sample.wav"
+SPEAKER = "Marcos Rudaski"
 
 
 class Generation(ThreadExecutor):
     def __init__(self):
         super().__init__()
-        config = XttsConfig()
-        config.load_json(f"{TTS_MODEL}/config.json")
-        self.model = Xtts.init_from_config(config)
-        self.model.load_checkpoint(config, checkpoint_dir=TTS_MODEL)
+        self.text_tokenizer = TextTokenizer(str(Path(TTS_MODEL) / "vocab.json"))
+        self.model = XTTSGPT(Path(TTS_MODEL) / "config.json")
+        self.model.load(Path(TTS_MODEL) / "model.pth")
         self.model.cuda()
+        self.model.set_speaker_embeddings(Path(TTS_MODEL) / "speakers_xtts.pth", SPEAKER)
 
-        self.gpt_cond_latent, self.speaker_embedding = self.model.get_conditioning_latents(audio_path=[SAMPLE])
-        self.sample_rate = self.model.config.audio.output_sample_rate
+    def blocking_fn(self, text: str, streams: Dict[str, StreamingConnection], id_: str, audio_config: dict) -> None:
+        token_encoding = self.text_tokenizer.encode(text, LANG)
+        input_ids = torch.tensor(
+            token_encoding.ids + [self.model.config.gpt_start_audio_token], dtype=torch.int64
+        ).unsqueeze(0)
+        streamer = Streamer(streams, id_, audio_config, [], self.model.decoder, self.model.speaker_emb, chunk_size=20)
+        self.model.generate(
+            input_ids.cuda(),
+            bos_token_id=self.model.config.gpt_start_audio_token,
+            pad_token_id=self.model.config.gpt_stop_audio_token,
+            eos_token_id=self.model.config.gpt_stop_audio_token,
+            do_sample=True,
+            top_p=0.85,
+            top_k=50,
+            temperature=0.75,
+            num_return_sequences=1,
+            num_beams=1,
+            length_penalty=1.0,
+            repetition_penalty=5.0,
+            max_new_tokens=self.model.config.gpt_max_audio_tokens,
+            return_dict_in_generate=True,
+            output_hidden_states=True,
+            all_latents=streamer.all_latents,
+            streamer=streamer
+        )
 
-    def blocking_fn(self):
-        try:
-            # StopIteration does not play well with async code. Therefore handle generator exhaustion gracefully.
-            x = next(self.inference_stream, None)
-            return x
-        except ValueError:
-            # TODO: Ideally this should not be necessary. It seems there can be multiple threads accessing
-            #  `inference_stream` at the same time, but not sure why.
-            return 'DUMMY'
 
-    def initialize_stream(self, text):
-        self.inference_stream = self.model.inference_stream(text, LANG, self.gpt_cond_latent, self.speaker_embedding)
+class Streamer(BaseStreamer):
+
+    def __init__(self, streams, id_, audio_config, all_latents, decoder, speaker_embedding, chunk_size):
+        self.streams = streams
+        self.all_latents = all_latents
+        self.chunk_size = chunk_size
+        self.decoder = decoder
+        self.speaker_embedding = speaker_embedding.to(decoder.device)
+        self.generated_tokens = []
+        self.len_generated_audio_bytes = 0
+        self.base_msg = {
+            "status": "GENERATING",
+            "id": id_,
+            "config": audio_config
+        }
+
+    def put(self, token):
+        self.generated_tokens.append(token)
+        if len(self.generated_tokens) % self.chunk_size == 0:
+            self.generate_audio()
+
+    def end(self):
+        self.generate_audio()
+
+    def generate_audio(self):
+        generated_latents = torch.cat(self.all_latents, dim=1)[:, -len(self.generated_tokens):]
+        with torch.no_grad():
+            generated_audio = self.decoder(generated_latents, g=self.speaker_embedding)
+
+        bytes_ = generated_audio.squeeze().cpu().numpy().tobytes()
+        self.streams['client'].send(self.base_msg | {'audio': bytes_[self.len_generated_audio_bytes:]})
+        self.len_generated_audio_bytes = len(bytes_)
 
 
 class TTSServer(BaseServer):
@@ -49,7 +98,7 @@ class TTSServer(BaseServer):
         self.audio_config = {
             'format': 1,  # 1 is pyaudio.paFloat32.
             'channels': 1,
-            'rate': self.generation.sample_rate
+            'rate': self.generation.model.decoder.output_sample_rate
         }
 
     def _get_cutoff_idx(self, received: List[Message.DataDict]) -> int:
@@ -66,21 +115,7 @@ class TTSServer(BaseServer):
     async def _run_workload(self, received: List[Message.DataDict]) -> None:
         text = ''.join(msg['text'] for msg in received)
         print(f"{text}\n---")
-        self.generation.initialize_stream(text)
-        while True:
-            chunk = await self.generation.run()
-            if chunk is None:
-                break
-            elif chunk == "DUMMY":
-                await asyncio.sleep(POLL_INTERVAL)
-                continue
-
-            self.streams['client'].send({
-                'audio': np.array(chunk.cpu(), dtype=np.float32).tobytes(),
-                'status': 'GENERATING',
-                'config': self.audio_config,
-                'id': received[0]['id']
-            })
+        await self.generation.run(text, self.streams, received[0]['id'], self.audio_config)
         if received[-1]['status'] == 'FINISHED':
             self.streams['client'].send({'audio': b'', 'status': 'FINISHED', 'id': received[0]['id']})
 
